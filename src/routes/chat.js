a60d48520d75bea3ab0f6b284ecade091136a6d6
Solargi <solarg@usi.ch>
2021-11-29 21:32:51 +0100

    added not implemented yet message to all not yet implemented routes

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -4,6 +4,7 @@ const router = express.Router()
 /* GET chat page. */
 router.get('/', function (req, res, next) {
   res.render('chat')
+  res.send('route not implemented yet')
 })
 
 // export the required modules
--- src/routes/exchange.js
+++ src/routes/exchange.js
@@ -9,16 +9,19 @@ router.get('/', function (req, res, next) {
 /* GET exchange token page. */
 router.get('/token', function (req, res, next) {
   // to complete
+  res.send('route not implemented yet')
 })
 
 /* GET exchange nft page. */
 router.get('/nft', function (req, res, next) {
   // to complete
+  res.send('route not implemented yet')
 })
 
 /* GET exchange collection page. */
 router.get('/collection', function (req, res, next) {
   // to complete
+  res.send('route not implemented yet')
 })
 
 // export the required modules
