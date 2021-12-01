41e742fb2ba45e15ffc1f772ba8ebb99a5979eb2
Solargi <solarg@usi.ch>
2021-12-01 22:43:53 +0100

    added db requests and modified first tests to use the db as well

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,33 +1,70 @@
 const express = require('express')
+const { MongoMissingCredentialsError, MongoCredentials } = require('mongodb')
 const router = express.Router()
-// ******* TO BE REMOVED/REPLACED/REVIEWD WHEN DATABASE/LONGIN IS READY 
-const users = []
-
-function createUser(id, username, password, email, name, surname, wallet, collection, friendlist, settings, ppic, bio, tracking) {
-  const user = {
-      '_id': id,
-      'username': username,
-      'password': password,
-      'email': email,
-      'name': name,
-      'surname': surname,
-      'wallet': wallet,
-      'collection': collection,
-      'friendlist': friendlist,
-      'settings': settings,
-      'ppic': ppic,
-      'bio': bio,
-      'tracking': tracking
-  }
-  return user
+// Imports for database
+const models = require('../models').model
+const ObjectId = require('mongodb').ObjectId
+// HELPER FUNCTIONS
+/**
+ * Function that emoves the sensitive data from a user object
+ * such as (password,email,name,surname,settings)
+ * @param {object} user the user from which we want to remove the sensitive data
+ * @retun {Undefined}
+ */
+function removeSensitiveData (user) {
+    delete user.password
+    delete user.email
+    delete user.name
+    delete user.surname
+    delete user.settings
+}
+function createUser (req) {
+    const user = {
+        //_id: req.body.id, provided from db
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        name: req.body.name,
+        surname: req.body.surname,
+        wallet: req.body.wallet,
+        collection: req.body.collection,
+        friendlist: req.body.friendlist,
+        settings: req.body.settings,
+        ppic: req.body.ppic,
+        bio: req.body.bio,
+        tracking: req.body.tracking
+    }
+    return user
 }
 
-function addUser (user) {
-  users.push(user)
-}
+// ******* TEST TO BE REMOVED/REPLACED/REVIEWD WHEN DATABASE/LONGIN IS READY
+// const users = []
 
-// const dummyUser = createUser('id', 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], {currency : 'eth', mode : 'dark'}, 'ppic', 'bio', [])
-// const dummyUser2 = createUser('id2', 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], {currency : 'eth', mode : 'dark'}, 'ppic', 'bio', [])
+// function createUserF (id, username, password, email, name, surname, wallet, collection, friendlist, settings, ppic, bio, tracking) {
+//     const user = {
+//         _id: id,
+//         username: username,
+//         password: password,
+//         email: email,
+//         name: name,
+//         surname: surname,
+//         wallet: wallet,
+//         collection: collection,
+//         friendlist: friendlist,
+//         settings: settings,
+//         ppic: ppic,
+//         bio: bio,
+//         tracking: tracking
+//     }
+//     return user
+// }
+
+// function addUser (user) {
+//     users.push(user)
+// }
+
+// const dummyUser = createUserF('id', 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], { currency: 'eth', mode: 'dark' }, 'ppic', 'bio', [])
+// const dummyUser2 = createUserF('id2', 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], { currency: 'eth', mode: 'dark' }, 'ppic', 'bio', [])
 // addUser(dummyUser)
 // addUser(dummyUser2)
 
@@ -35,57 +72,156 @@ function addUser (user) {
 
 /* GET user page. */
 router.get('/', function (req, res, next) {
-  res.render('user')
+    models.users.find().toArray().then(result=>{
+        const user = result;
+        if (result === undefined) {
+            res.status(404).end();
+        } else if (req.accepts('html')) {
+            res.render('user', { user })
+        } else if (req.accepts('application/json')) {
+            res.status(200).json(result)
+        } else {
+            res.status(406).end()
+        }
+    })
 })
 
-// render single user?
+/* Get single user */
 router.get('/:_id', function (req, res, next) {
-  let result = users
-  result = result.filter(user => user._id === req.params._id)
-  console.log('this is the user_id')
-  console.log(result)
-  if (result.length === 1) {
-    res.status(200).json(result[0])
-  } else {
-    res.status(404).end()
-  }
+    if (req.accepts('application/json')) {
+        try {
+        const filter =  { _id: new ObjectId(req.params._id)} 
+        } catch (e) {res.status(404).end}
+        models.users.findOne(filter).then(result=>{
+            const user = result
+            if (result === undefined) {
+                res.status(404).end();
+            } else {
+                res.json(result)
+            }
+        }).catch(err => { console.log(err) })
+    } else {
+        res.status(406).end()
+    }
 })
 
 /* GET user settings page. */
-router.get('/settings', function (req, res, next) {
-  // to complete
-  res.send('route not implemented yet')
+router.get('/settings/:_id', function (req, res, next) {
+    const filter =  { _id: new ObjectId(req.params._id) };
+    models.users.findOne(filter).then(result=>{
+        const user = result
+        if (user === null) {
+            res.status(404).end();
+        } else if (req.accepts('application/json')) {
+            res.status(200).json(user.settings)
+        } else {
+            res.status(406).end()
+        }
+    })
 })
 
 /* GET user assets page. */
-router.get('/assets', function (req, res, next) {
-  // to complete
-  res.send('route not implemented yet')
+router.get('/assets/:_id', function (req, res, next) {
+    const filter =  { _id: new ObjectId(req.params._id) };
+    models.users.findOne(filter).then(result=>{
+        const user = result
+        if (user === undefined) {
+            res.status(404).end();
+        } else if (req.accepts('application/json')) {
+            res.status(200).json(user.collection)
+        } else {
+            res.status(406).end()
+        }
+    })
 })
 
 /* GET user friends page. */
-router.get('/friends', function (req, res, next) {
-  // to complete
-  res.send('route not implemented yet')
+router.get('/friends/:_id', function (req, res, next) {
+    const filter =  { _id: new ObjectId(req.params._id) };
+    models.users.findOne(filter).then(result=>{
+        const user = result
+        if (user === undefined) {
+            res.status(404).end();
+        } else if (req.accepts('application/json')) {
+            res.status(200).json(user.friendlist)
+        } else {
+            res.status(406).end()
+        }
+    })
 })
 
 /* GET user recently viewed assets page. */
-router.get('/recently_viewed', function (req, res, next) {
-  // to complete
-  res.send('route not implemented yet')
+router.get('/recently_viewed/:_id', function (req, res, next) {
+// to complete
+    res.send('route not implemented yet')
 })
 
 /* GET user following. */
-router.get('/following', function (req, res, next) {
-  // to complete
-  res.send('route not implemented yet')
+router.get('/following/:_id', function (req, res, next) {
+    const filter =  { _id: new ObjectId(req.params._id) };
+    models.users.findOne(filter).then(result=>{
+        const user = result
+        if (user === undefined) {
+            res.status(404).end();
+        } else if (req.accepts('application/json')) {
+            res.status(200).json(user.tracking)
+        } else {
+            res.status(406).end()
+        }
+    })
 })
 
 /* GET user edit page. */
-router.get('/edit', function (req, res, next) {
-  // to complete
-  res.send('route not implemented yet')
+router.get('/edit/:_id', function (req, res, next) {
+    const filter =  { _id: new ObjectId(req.params._id) };
+    models.users.findOne(filter).then(result=>{
+        const user = result
+        if (user === undefined) {
+            res.status(404).end();
+        } else if (req.accepts('application/json')) {
+            res.status(200).json(user)
+        } else {
+            res.status(406).end()
+        }
+    })
 })
+
+/* post user , requires form. */
+router.post('/', function (req, res, next) {
+    const user = createUser(req)
+    models.users.insertOne(user).then(result => {
+        res.status('200')
+    })
+})
+
+/* Put(edit) User , requires form. */
+router.put('/:id', function (req, res, next) {
+    const user = createUser(req)
+    const filter = { _id: new ObjectId(req.params._id) };
+    models.users.replaceOne(filter, user, { upsert: true }) // update + insert = upsert
+        .then(result => {
+            const found = (result.upsertedCount === 0);
+            res.status(found ? 200 : 201).json(user);
+        });
+})
+
+
+/* Delete Uses */ //TO BE COMPLETED
+router.delete('/:_id', function (req, res) {
+  const filter = { _id: new ObjectId(req.params._id) };
+  models.users.findOneAndDelete(filter).then(result => {
+      if (result.value == null) {
+          res.status(404).end();
+      } else {
+          if (req.accepts('application/json')) {
+            res.status(200).json(result)
+          } else {
+              res.status(406).end();
+          }
+      }
+  });
+});
+
 
 // export the required modules
 module.exports = router
--- src/test/userTest.js
+++ src/test/userTest.js
@@ -1,72 +1,203 @@
 const assert = require('chai').assert
 const expect = require('chai').expect
+const rewire = require('rewire')
+const userJs = rewire('../routes/user.js')
+const models = require('../models').model
+const ObjectId = require('mongodb').ObjectId
+const express = require('express');
+const router = express.Router();
 // const describe = require('chai').describe
 const request = require('supertest')('http://localhost:3000')
-const users = [] //array of user
+//const users = [] // array of user
 
-function createUser(id, username, password, email, name, surname, wallet, collection, friendlist, settings, ppic, bio, tracking) {
-    const user = {
-        '_id': id,
-        'username': username,
-        'password': password,
-        'email': email,
-        'name': name,
-        'surname': surname,
-        'wallet': wallet,
-        'collection': collection,
-        'friendlist': friendlist,
-        'settings': settings,
-        'ppic': ppic,
-        'bio': bio,
-        'tracking': tracking
-    }
-    return user
+//setting up test data in db
+function createUser (id, username, password, email, name, surname, wallet, collection, friendlist, settings, ppic, bio, tracking) {
+  const user = {
+    //_id: id,
+    username: username,
+    password: password,
+    email: email,
+    name: name,
+    surname: surname,
+    wallet: wallet,
+    collection: collection,
+    friendlist: friendlist,
+    settings: settings,
+    ppic: ppic,
+    bio: bio,
+    tracking: tracking
+  }
+  return user
 }
 
-function addUser (user) {
-    users.push(user)
-}
+// function addUserDb(user){
+//   const filter = { _id: user._id };
+//   models.users.replaceOne(filter, user, { upsert: true }) // update + insert = upsert
+// // .then(result => {
+// //     const found = (result.upsertedCount === 0);
+// //     res.status(found ? 200 : 201).json(user);
+// // });
+// }
 
-const dummyUser = createUser('id', 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], {currency : 'eth', mode : 'dark'}, 'ppic', 'bio', [])
-addUser(dummyUser)
+// function getUserDb(user_id){
+//   const filter =  { _id: user._id };
+//     models.users.findOne(filter).then(result=>{
+//         console.log(result)
+//     })
+// }
 
+// function addUser (user) {
+//   users.push(user)
+// }
+// create users for tests
+const dummyUser = createUser( 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], { currency: 'eth', mode: 'dark' }, 'ppic', 'bio', [])
+const dummyUser2 = createUser( 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], { currency: 'eth', mode: 'dark' }, 'ppic', 'bio', [])
 const _id = dummyUser._id
 
-describe('Testing user routes', function () {
-    describe('GET /user/:_id', function () {
-        it('the user metadata should be found', function (done) {
-            request
-                .get('/user/' + _id)
-                .set('Accept', 'application/json')
-                .send()
-                .expect(200)
-                .expect('Content-Type', /json/, 'it should respond with Content-Type: application/json')
-                .end(function (err, res) {
-                    if (err) return done(err)
-                    const user = JSON.parse(res.text)
-                    expect(user._id).to.equal(_id)
-                    expect(user.username).to.equal('username')
-                    expect(user.password).to.equal('password')//probably we don't want this
-                    expect(user.email).to.equal('email')
-                    expect(user.name).to.equal('name')
-                    expect(user.surname).to.equal('surname')
-                    expect(user.wallet).to.equal('wallet')
-                    expect(user.collection).to.be.an('array').that.is.empty
-                    expect(user.friendlist).to.be.an('array').that.is.empty
-                    expect(user.settings).to.include({currency: 'eth', mode: 'dark' })
-                    expect(user.ppic).to.equal('ppic')
-                    expect(user.bio).to.equal('bio')
-                    expect(user.tracking).to.be.an('array').that.is.empty
-                    done()
-                })
-        })
-        it('a random user should be not found ', function (done) {
+// add users to db
 
-            request
-                .get('/user/' + Math.random())
-                .send()
-                .expect(404, done);
-        });
+describe('Connecting to database', function () {
+  before(function (done) {
+    let attempts = 0
+  async function check () {
+    if (models.db && models.users) {
+      console.log("connected")
+      console.log(dummyUser)
+      models.users.insertOne(dummyUser).then(function(){models.users.insertOne(dummyUser2)})
+      .then(function(){done()}).catch(err=>{console.log(err)})
+    } else {
+      console.log('Trying to connect')
+      attempts++;
+      if (attempts < 10) {
+        setTimeout(check, 500)
+      } else {
+        throw new Error('Unable to connect the test to the database')
+      }
+    }
+  }
+  
+  check()
+})
+  // describe('Testing remove sensitive data function', function () {
+  //   it('successfully deleted the sensitive info', function (done) {
+  //     // create copy of the object
+  //     const user = createUser('id', 'username', 'password', 'email', 'name', 'surname', 'wallet', [], [], { currency: 'eth', mode: 'dark' }, 'ppic', 'bio', [])
+  //     // check copy has every field before deletion
+  //     expect(user._id).to.equal('id')
+  //     expect(user.username).to.equal('username')
+  //     expect(user.password).to.equal('password')
+  //     expect(user.email).to.equal('email')
+  //     expect(user.name).to.equal('name')
+  //     expect(user.surname).to.equal('surname')
+  //     expect(user.wallet).to.equal('wallet')
+  //     expect(user.collection).to.be.an('array').that.is.empty
+  //     expect(user.friendlist).to.be.an('array').that.is.empty
+  //     expect(user.settings).to.include({ currency: 'eth', mode: 'dark' })
+  //     expect(user.ppic).to.equal('ppic')
+  //     expect(user.bio).to.equal('bio')
+  //     expect(user.tracking).to.be.an('array').that.is.empty
+
+  //     // deletion and check after
+
+  //     const removeSensitiveData = userJs.__get__('removeSensitiveData') // get the removesdata function form users.js
+  //     removeSensitiveData(user)
+  //     expect(user._id).to.equal(_id)
+  //     expect(user.username).to.equal('username')
+  //     expect(user.password).to.equal(undefined)
+  //     expect(user.email).to.equal(undefined)
+  //     expect(user.name).to.equal(undefined)
+  //     expect(user.surname).to.equal(undefined)
+  //     expect(user.wallet).to.equal('wallet')
+  //     expect(user.collection).to.be.an('array').that.is.empty
+  //     expect(user.friendlist).to.be.an('array').that.is.empty
+  //     expect(user.settings).to.equal(undefined)
+  //     expect(user.ppic).to.equal('ppic')
+  //     expect(user.bio).to.equal('bio')
+  //     expect(user.tracking).to.be.an('array').that.is.empty
+  //     done()
+  //   })
+  // })
+
+  describe('Testing user routes', function () {
+    describe('GET /user/_id', function () {
+      it('the user metadata should be found', function (done) {
+        models.users.findOne({username:'password'}).then(result=>{request
+          .get('/user/'+result._id)
+          .set('Accept', 'application/json')
+          .send()
+          .expect(200)
+          .expect('Content-Type', /json/, 'it should respond with Content-Type: application/json')
+          .end(function (err, res) {
+            if (err) return done(err)
+            const user = JSON.parse(res.text)
+            console.log('fetched user=')
+            console.log(user)
+            expect(user._id).to.equal('32')
+            expect(user.username).to.equal('username')
+            expect(user.password).to.equal('password')// probably we don't want this
+            expect(user.email).to.equal('email')
+            expect(user.name).to.equal('name')
+            expect(user.surname).to.equal('surname')
+            expect(user.wallet).to.equal('wallet')
+            expect(user.collection).to.be.an('array').that.is.empty
+            expect(user.friendlist).to.be.an('array').that.is.empty
+            expect(user.settings).to.include({ currency: 'eth', mode: 'dark' })
+            expect(user.ppic).to.equal('ppic')
+            expect(user.bio).to.equal('bio')
+            expect(user.tracking).to.be.an('array').that.is.empty
+          })}).then(function(){done()})
+        
+      })
+
+      it('a random user should be not found ', function (done) {
+        request
+          .get('/user/' + Math.random())
+          .send()
+          .expect(404, done)
+      })
+
+      it(' the request do not accept json -> bad request', function (done) {
+        request
+          .get('/user/' + Math.random())
+          .set('Accept', 'html')
+          .send()
+          .expect(406, done)
+      })
     })
 
-})
+    describe('GET /user/', function () {
+      it('the users metadata should be found', function (done) {
+        request
+          .get('/user/')
+          .set('Accept', 'application/json')
+          .send()
+          .expect(200)
+          .expect('Content-Type', /json/, 'it should respond with Content-Type: application/json')
+          .end(function (err, res) {
+            if (err) return done(err)
+            const fUsers = JSON.parse(res.text)
+            console.log('fUsers')
+            console.log(fUsers)
+            expect(JSON.stringify(fUsers)).to.equal(JSON.stringify(users))
+            done()
+          })
+      })
+    })
+
+
+  })
+  })
+
+// // addUserDb(dummyUser)
+// // addUserDb(dummyUser2)
+// // add users to db
+// console.log('here')
+// // getUserDb(dummyUser._id)
+// // getUserDb(dummyUser2._id)
+
+
+
+// console.log('models.users')
+// console.log(models.users)
+
+
