d31482ff8ca8bc078725eaefa72477b529f91821
dipiee <dipiee@usi.ch>
2021-12-01 21:49:10 +0100

    Implemented methods in routes.js

--- src/public/js/routes.js
+++ src/public/js/routes.js
@@ -1,10 +1,25 @@
+const axios = require('axios').default;
+require('dotenv').config();
+
 /**
  * Function to get the general data of a collection giving a collection slug.
  * @param string slug, the collection slug
  * @returns {object} object with all the data.
  */
 function getCollectionData (slug) {
-    // to be implemented
+    const options = {
+        method: 'GET',
+        url: 'https://api.opensea.io/api/v1/collection/' + slug,
+        headers: { Accept: 'application/json', 'X-API-KEY': process.env.OPENSEA_API }
+    }
+    return Promise.resolve(
+        axios.request(options).then(function (response) {
+            // console.log(response.data);
+            return response.data;
+        }).catch(function (error) {
+            console.error(error);
+        })
+    )
 }
 
 /**
@@ -15,7 +30,20 @@ function getCollectionData (slug) {
  * @returns {object} object with all the data.
  */
 function getSalesFromTimeToTime (contractAddress, firstTimestamp, secondTimestamp) {
-    // to be implemented
+    const options = {
+        method: 'GET',
+        url: 'https://api.opensea.io/api/v1/events?asset_contract_address=' + contractAddress + '&event_type=successful&only_opensea=false&offset=0&occurred_after=' + firstTimestamp + '&occurred_before=' + secondTimestamp + '&limit=250',
+        headers: { Accept: 'application/json', 'X-API-KEY': process.env.OPENSEA_API }
+    }
+
+    return Promise.resolve(
+        axios.request(options).then(function (response) {
+            // console.log(response.data);
+            return response.data;
+        }).catch(function (error) {
+            console.error(error);
+        })
+    )
 }
 
 /**
@@ -26,5 +54,18 @@ function getSalesFromTimeToTime (contractAddress, firstTimestamp, secondTimestam
  * @returns {object} object with data of time and sell price [{ time: 'time', price: 'price'}].
  */
 function createArrayWithPrices (contractAddress, firstTimestamp, secondTimestamp) {
-    // to be implemented
+    return Promise.resolve(
+        getSalesFromTimeToTime(contractAddress, firstTimestamp, secondTimestamp)
+            .then(res => {
+                const data = [];
+                res.asset_events.forEach(el => {
+                    data.push({
+                        timestamp: el.transaction.timestamp,
+                        price: (el.total_price / 1000000000000000000)
+                    });
+                })
+                // console.log(data);
+                return data;
+            })
+    );
 }
--- src/test/routesTest.js
+++ src/test/routesTest.js
@@ -1,44 +1,55 @@
 const rewire = require('rewire')
 const routesFunc = rewire('../public/js/routes.js')
 const assert = require('chai').assert
-const expect = require('chai').expect
+// const expect = require('chai').expect
+// const axios = require('axios').default;
+require('dotenv').config();
 
 const currentDate = new Date();
 const currentTimestamp = Math.trunc(currentDate.getTime() / 1000);
 
 
 describe('Testing fetching general data of a collection', function () {
-        it('Using getCollectionData with collection slug', function (done) {
+    it('Using getCollectionData with collection slug', function (done) {
 
-            const getCollectionData = routesFunc.__get__('getCollectionData')
-            const data = getCollectionData('doodles-official')
-
-            expect(data.collection).to.be.not.undefined
-            expect(data.stats).to.be.not.undefined
-            done()
-        });
-})
-
-describe('Testing fetching sales data of a collection', function () {
-    it('Get sales data using the collection address and two timestamps', function (done) {
+        const getCollectionData = routesFunc.__get__('getCollectionData')
         
-        const getSalesFromTimeToTime = routesFunc.__get__('getSalesFromTimeToTime')
-        const data = getSalesFromTimeToTime('0x1a92f7381b9f03921564a437210bb9396471050c', currentTimestamp - 86400 * 1, currentTimestamp - 86400 * 0)
-
-        expect(data.transaction).to.be.not.undefined
-        expect(data.total_price).to.be.not.undefined
-        done()
+        getCollectionData('doodles-official')
+        .then(data => {
+            assert(data.collection)
+            assert(data.collection.stats)
+            done()
+        })
     });
 })
 
 describe('Testing fetching sales data of a collection', function () {
-    it('Get general data using the collction slug', function (done) {
+    // this.timeout(15000);
+    it('Get sales data using the collection address and two timestamps', function (done) {
+        
+        const getSalesFromTimeToTime = routesFunc.__get__('getSalesFromTimeToTime')
+        
+        getSalesFromTimeToTime('0x1a92f7381b9f03921564a437210bb9396471050c', currentTimestamp - 86400 * 1, currentTimestamp - 86400 * 0)
+        .then(data => {
+            assert(data.transaction)
+            assert(data.total_price)
+        })
+        done()
+    });
+})
+
+describe('Testing creation of data to plot the graphs', function () {
+    it('Create the array of objects with data for the graphs', function (done) {
         
         const createArrayWithPrices = routesFunc.__get__('createArrayWithPrices')
-        const data = createArrayWithPrices('0x1a92f7381b9f03921564a437210bb9396471050c', currentTimestamp - 86400 * 1, currentTimestamp - 86400 * 0)
 
-        expect(data.timestamp).to.be.not.undefined
-        expect(data.price).to.be.not.undefined
-        done()
+        createArrayWithPrices('0x1a92f7381b9f03921564a437210bb9396471050c', currentTimestamp - 86400 * 1, currentTimestamp - 86400 * 0)
+        .then(data => {
+            data.forEach(obj => {
+                assert(obj.timestamp)
+                assert(obj.price)
+            })
+            done()
+        })
     });
 })
