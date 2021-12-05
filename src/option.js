4ba96ab6f6f7417f0b9873a001379525d6504090
Solargi <solarg@usi.ch>
2021-12-05 21:47:26 +0100

    refactored some options.js methods and implemented route GET graph/types, worked with Enrico Di Pietro

--- src/option.js
+++ src/option.js
@@ -11,7 +11,7 @@ const lastMidnightTimestamp = Math.trunc(lastMidnight.getTime() / 1000);
  * @param int timeInDays, the number of the days you want to get the volume of (e.g. 7 means the last 7 days).
  * @returns {array} object option that is used to draw the chart.
  */
-function getOptionForBarChart (contractAddress, timeInDays) {
+async function getOptionForBarChart (contractAddress, timeInDays) {
     const dateArray = []
     for (let i = timeInDays; i > 0; i--) {
         const date = new Date((lastMidnightTimestamp - (86400 * i)) * 1000)
@@ -24,31 +24,33 @@ function getOptionForBarChart (contractAddress, timeInDays) {
     const day = date.getDate()
     dateArray.push(month + '/' + day)
 
-    dailyVolume(contractAddress, timeInDays)
-        .then(dailyVolumeArray => {
-            const option = {
-                xAxis: {
-                    type: 'category',
-                    data: dateArray
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: [
-                    {
-                        data: dailyVolumeArray,
-                        type: 'bar',
-                        showBackground: true,
-                        backgroundStyle: {
-                            color: 'rgba(180, 180, 180, 0.2)'
-                        }
+    let dailyVolumeArray;
+    let option;
+    try {
+        dailyVolumeArray = await dailyVolume(contractAddress, timeInDays)
+
+        option = {
+            xAxis: {
+                type: 'category',
+                data: dateArray
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: dailyVolumeArray,
+                    type: 'bar',
+                    showBackground: true,
+                    backgroundStyle: {
+                        color: 'rgba(180, 180, 180, 0.2)'
                     }
-                ]
-            };
-            console.log(option.xAxis.data)
-            console.log(option.series[0].data)
-            return option
-        });
+                }
+            ]
+        }
+    } catch (error) { console.error(error); }
+
+    return option
 }
 
 /**
@@ -57,35 +59,39 @@ function getOptionForBarChart (contractAddress, timeInDays) {
  * @param int timeInDays, the number of the days you want to get the volume of (e.g. 7 means the last 7 days).
  * @returns {array} object option that is used to draw the chart.
  */
-function getOptionForScatterChart (contractAddress, timeInDays) {
+async function getOptionForScatterChart (contractAddress, timeInDays) {
     const startTimestamp = currentTimestamp - 86400 * timeInDays;
     const endTimestamp = currentTimestamp;
 
-    createArrayWithPrices(contractAddress, startTimestamp, endTimestamp)
-        .then(timePriceArray => {
-            const plottedTimePriceArray = [];
-            timePriceArray.forEach(data => {
-                plottedTimePriceArray.push(
-                    [data.timestamp, data.price]
-                )
-            })
+    let timePriceArray;
+    let option;
+    try {
+        timePriceArray = await createArrayWithPrices(contractAddress, startTimestamp, endTimestamp)
 
-            const option = {
-                xAxis: {
-                    type: 'time'
-                },
-                yAxis: {},
-                series: [
-                    {
-                        symbolSize: 10,
-                        data: plottedTimePriceArray,
-                        type: 'scatter'
-                    }
-                ]
-            }
+        const plottedTimePriceArray = [];
+        timePriceArray.forEach(data => {
+            plottedTimePriceArray.push(
+                [data.timestamp, data.price]
+            )
+        })
 
-            return option
-        });
+        option = {
+            xAxis: {
+                type: 'time'
+            },
+            yAxis: {},
+            series: [
+                {
+                    symbolSize: 10,
+                    data: plottedTimePriceArray,
+                    type: 'scatter'
+                }
+            ]
+        }
+    } catch (error) { console.error(error); }
+
+    return option
 }
 
 module.exports.getOptionForBarChart = getOptionForBarChart;
+module.exports.getOptionForScatterChart = getOptionForScatterChart;
--- src/routes/graph.js
+++ src/routes/graph.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const getOptionForBarChart = require('../option.js').getOptionForBarChart
+const getOptionForScatterChart = require('../option.js').getOptionForScatterChart
 
 /* GET graph page. */
 router.get('/', function (req, res, next) {
@@ -7,9 +9,24 @@ router.get('/', function (req, res, next) {
 })
 
 /* GET the graph types. */
-router.get('/types', function (req, res, next) {
-    // to complete
-    res.send('route not implemented yet')
+router.get('/types', async function (req, res, next) {
+    const chartType = req.query.chart
+    const contractAddress = req.query.address
+    const time = req.query.time
+
+    if (req.accepts('json')) {
+        if (chartType === 'bar') {
+            const option = await getOptionForBarChart(contractAddress, time);
+            res.status(200).send(option);
+        } else if (chartType === 'scatter') {
+            const option = await getOptionForScatterChart(contractAddress, time)
+            res.status(200).send(option);
+        } else {
+            res.status(404).end()
+        }
+    } else {
+        res.status(406).end()
+    };
 })
 
 /* GET the graph data types. */
