7db72b37a0c427741de999fb115b7456a4f6fbd9
paolaguma <paola.guma@usi.ch>
2021-12-05 20:08:47 +0100

    updated app.js and added dashboard and follow

--- src/app.js
+++ src/app.js
@@ -47,6 +47,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // routers
 app.use('/', routers.dashboard)
+app.use('/home', routers.dashboard)
 app.use('/graph', routers.graph)
 app.use('/chat', routers.chat)
 app.use('/user', routers.chat)
