4fb9709f397f9209e1e4d124f827f56c92f597cc
Solargi <solarg@usi.ch>
2021-12-08 00:28:29 +0100

    working on friends requests with sockets

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,13 +1,29 @@
-let socket = io();
+const socket = io();
 
 
 // when the connection is active
-socket.on("connect", () =>{
-    console.log("Browser connected :)");
+socket.on('connect', () =>{
+    console.log('Browser connected :');
 });
 
 // disconnected
 socket.on("disconnect", () => {
-    console.log("Browser disconnected :(");
+    console.log('Browser disconnected');
 });
 
+function init_client() {
+    // button in the html page to send the friend request (one for each user showed)
+    const addFriendButton = document.getElementById('addFriend')
+    // when the button is clicked we use the event to get the receiver username from the html field
+    addFriendButton.addEventListener('click', (event)=>{
+        // create friend Request object to send with sender username, receiver username,
+        // event.target.tagName where tag name user is the field with the friend request target username
+        // passing the button so that it can change from add to request sent.
+        const friendRequest = { sender: req.user, receiver: event.target.user, addFriendButton: addFriendButton }
+        socket.emit('friend.request.sent', friendRequest)
+    })
+
+    socket.on('request.sent', addFriendButton=>{
+        addFriendButton.innerHTML = 'request pending'
+    })
+}
--- src/ws.js
+++ src/ws.js
@@ -1,21 +1,56 @@
 const io = require('socket.io')();
-const EventEmitter = require('events');
-const eventBus = new EventEmitter();
+// const EventEmitter = require('events');
+// const eventBus = new EventEmitter();
 
-const models = require('./models').model; 
-const ObjectId = require('mongodb').ObjectId;
+const models = require('./models').model;
+
+
 
 function init (server) {
-    console.log("Starting Web Server");
-    io.attach(server);
-    io.on('connection', function (socket) {
-        console.log("SOCKET ID: "+socket.id);
+    console.log('Starting Web Server');
 
+    io.attach(server);
+
+    io.on('connection', function (socket) {
+        console.log('SOCKET ID:' + socket.id)
         socket.on('disconnect', function () {
             console.log('client disconnected id:', socket.id);
-        });
-    });
+        })
+        // receive and handle friend request
+        io.on('friend.request.sent', friendRequest=>{
+            // look for the friend request target and add request to his request list
+            const receiver = friendRequest.receiver
+            const sender = friendRequest.sender
+            let addFriendButton = friendRequest.addFriendButton
+            let filter
+            let user
+            try {
+                filter =  { username: receiver }
+            } catch (e) { console.log(e) }
+            models.users.findOne(filter).then(result=>{
+                user = result
+                if (user === null) {
+                    console.log('user not found')
+                }
+            }).then(function () {
+                // add friend request and store changes on db
+                user.friendrequests.push(sender)
+                filter = { username: friendRequest.sender };
+                models.users.replaceOne(filter, user, { upsert: true })
+                    .then(result => {
+                        // changing button text once the request has been sent
+                        if (result !== undefined) {
+                            addFriendButton = { 'addFriendButton': addFriendButton }
+                            socket.emit('request.sent', addFriendButton)
+                            console.log('friend request from ' + sender + 'added to ' + receiver + 'pending requests')
+                        } else {
+                            console.log('receiver: ' + receiver + 'not found')
+                        }
+                    });
+            })
+        })
+    })
 }
 
-module.exports.eventBus = eventBus;
+// module.exports.eventBus = eventBus;
 module.exports.init = init;
