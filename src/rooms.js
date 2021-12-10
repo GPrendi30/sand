2d4390cb06d49bdbde35abacd86a6ecb5d42f7b2
Viky <scoccv@usi.ch>
2021-12-10 02:50:05 +0100

    Implemented all methods in Room class. Added join request functionality.

--- src/rooms.js
+++ src/rooms.js
@@ -7,15 +7,14 @@ class Room {
      * @param {string} [icon] - path to room icon img
      */
     constructor (authorId, name, desc = '', users = [], icon = '') {
-        this._id = 0; // TODO: room id generation
         this.name = name;
-        this.created = Date.now();
         this.desc = desc;
         this.icon = icon; // TODO: Should generate room icon if not set
         this.author = authorId;
         this.admins = [authorId];
-        this.users = users.push(authorId);
-        // TODO: Store sent and received room join requests
+        this.users = users;
+        this.users.push(authorId); // add author to users as well.
+        this.joinRequests = []; // users that requested to join room
     }
 
     // Storage Functions
@@ -25,7 +24,19 @@ class Room {
      * @returns {JSON} - json string with room data
      */
     toJSON () {
-        // TODO
+        const json = JSON.stringify(
+            {
+                _id: this._id,
+                name: this.name,
+                created: this.created,
+                desc: this.desc,
+                icon: this.icon,
+                author: this.author,
+                admins: this.admins,
+                users: this.users
+            }
+        );
+        return json;
     }
 
     /**
@@ -33,7 +44,20 @@ class Room {
      * @param {JSON} json - json string with room data
      */
     loadJSON (json) {
-        // TODO
+        let data = {};
+        try {
+            data = JSON.parse(json);
+            if (data !== undefined) {
+                this.setName(data.name);
+                this.setDescription(data.desc);
+                this.setIcon(data.icon);
+                this.author = data.author;
+                this.admins = data.admins;
+                this.users = data.users;
+            }
+        } catch (e) {
+            data = this;
+        }
     }
 
     // User Handling
@@ -41,18 +65,34 @@ class Room {
 
     /**
      * Add user to room
+     * @param {string} reqId - id of user making add request
      * @param {string} newUserId - id of added user
      */
-    addUser (newUserId) {
-        // TODO
+    addUser (reqId, newUserId) {
+        if (this.isAdmin(reqId) && !this.isMember(newUserId)) {
+            this.users.push(newUserId);
+        }
     }
 
     /**
      * Remove user from room
-     * @param {string} user - id of removed user
+     * @param {string} reqId - id of user making remove request
+     * @param {string} userId - id of removed user
      */
-    removeUser (userId) {
-        // TODO
+    removeUser (reqId, userId) {
+        const userIsMember = this.isMember(userId); // deleting actual room member
+        const reqIsAuthor = this.isAuthor(reqId); // author can remove everyone
+        const adminDeletingUser = this.isAdmin(reqId) && !this.isAdmin(userId);
+
+        if (userIsMember && (reqIsAuthor || adminDeletingUser)) {
+            const idx = this.users.indexOf(userId);
+            this.users.splice(idx, 1);
+
+            if (this.isAdmin(userId)) {
+                const idx = this.admins.indexOf(userId);
+                this.admins.splice(idx, 1);
+            }
+        }
     }
 
     /**
@@ -61,24 +101,32 @@ class Room {
      * @returns {boolean} whether user is admin or not
      */
     isAdmin (userId) {
-        // TODO
-        return false;
+        return this.admins.includes(userId);
     }
 
     /**
      * Set user as admin
+     * @param {string} reqId - id of user making request
      * @param {string} userId - user id
      */
-    setAsAdmin (userId) {
-        // TODO
+    setAsAdmin (reqId, userId) {
+        if (this.isAdmin(reqId) && this.isMember(userId) && !this.isAdmin(userId)) {
+            this.admins.push(userId);
+        }
     }
 
     /**
      * Remove admin privileges from user
+     * @param {string} reqId - id of user making request
      * @param {string} userId - user id
      */
-    removeAdmin (userId) {
-        // TODO
+    removeAdmin (reqId, userId) {
+        const isAdmin = this.isAdmin(userId);
+        const reqHasPrivileges = this.isAuthor(reqId);
+        if (isAdmin && reqHasPrivileges) {
+            const idx = this.admins.indexOf(userId);
+            this.admins.splice(idx, 1);
+        }
     }
 
     /**
@@ -87,8 +135,7 @@ class Room {
      * @returns {boolean} - whether a given user is a member of this room
      */
     isMember (userId) {
-        // TODO
-        return false;
+        return this.users.includes(userId);
     }
 
     /**
@@ -97,8 +144,7 @@ class Room {
      * @returns {boolean} whether user is the creator or not
      */
     isAuthor (userId) {
-        // TODO
-        return false;
+        return this.author === userId;
     }
 
     /**
@@ -106,51 +152,87 @@ class Room {
      * @returns {integer} - id of the given room
      */
     getRoomId () {
-        // TODO: Set working room id format
-        return 0;
+        return this._id;
     }
-
 
     /**
      * Get size of room (ie, the number of users it has, including author)
      * @returns {integer} - room size
      */
     getRoomSize () {
-        // TODO
-    }
-
-    /**
-     * Set room size
-     * @param {int} roomSize - size of the room
-     */
-    setRoomSize (roomSize) {
-        // TODO
+        return this.users.length;
     }
 
     // Room Settings
+    // Should check if user has privileges for a given action
 
     /**
      * Set room icon
      * @param {string} newIcon - new room icon
+     * @param {string} reqId - id of user making request
      */
-    setIcon (newIcon) {
-        // TODO
+    setIcon (reqId, newIcon) {
+        if (this.isAdmin(reqId)) {
+            this.icon = newIcon;
+        }
     }
 
     /**
      * Set room name
      * @param {string} newName - new room name
+     * @param {string} reqId - id of user making request
      */
-    setName (newName) {
-        // TODO
+    setName (reqId, newName) {
+        if (this.isAdmin(reqId)) {
+            this.name = newName;
+        }
     }
 
     /**
      * Set room description
      * @param {string} newDesc - new room description
+     * @param {string} reqId - id of user making request
      */
-    setDescription (newDesc) {
-        // TODO
+    setDescription (reqId, newDesc) {
+        if (this.isAdmin(reqId)) {
+            this.desc = newDesc;
+        }
+    }
+
+
+    // Joining room
+
+    /**
+     * Make a request to join the room
+     * @param {string} userId - id of user who wants to join
+     */
+    requestToJoin (userId) {
+        if (!this.isMember(userId) && !this.isRequestingToJoin(userId)) {
+            this.joinRequests.push(userId);
+        }
+    }
+
+    /**
+     * Check if user has sent a join request
+     * @param {string} userId - id of user who (may have) sent join request
+     * @returns {boolean} - whether a user has sent a join request for this room
+     */
+    isRequestingToJoin (userId) {
+        return this.joinRequests.includes(userId);
+    }
+
+    /**
+     * Accept join request
+     * @param {string} userId - id of user who sent join request
+     */
+    acceptJoinRequest (userId) {
+        if (this.isRequestingToJoin(userId)) {
+            // remove user from request array
+            const idx = this.joinRequests.indexOf(userId);
+            this.joinRequests.splice(idx, 1);
+            // add user to users list
+            this.users.push(userId)
+        }
     }
 }
 
