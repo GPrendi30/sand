c736842e4b3b7b877798519851427385f6d162ed
Viky <scoccv@usi.ch>
2021-12-18 20:45:58 +0100

    Fixed way users were being displayed in single_room

--- src/public/js/routes.js
+++ src/public/js/routes.js
@@ -47,14 +47,8 @@ function getHome() {
 
     const main = document.querySelector('main');
 
-    main.innerHTML = ejs.src_views_settings({
-        result: {
-            _id: 0
-        }
-    });
-
-    // main.innerHTML = ejs.src_views_index(
-    //     { friends: [{ name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }] });
+    main.innerHTML = ejs.src_views_index(
+        { friends: [{ name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }, { name: 'not-geri' }] });
 
     /*
     NOTHING TO FETCH FOR THE MOMENT
